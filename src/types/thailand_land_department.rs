63ff455f0c0755490fef9a66cdca6d29ace3fa2c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-04-10 20:15:23 +0700

    fix: add cue to tools

--- src/types/thailand_land_department.rs
+++ src/types/thailand_land_department.rs
@@ -2,8 +2,8 @@ use derive_getters::Getters;
 use derive_more::{From, Into};
 use derive_new::new;
 
-/// TODO: When the land is registered, the land department issues a Chanot
-/// TODO: When a condo unit is registered, the land department issues a Chanot as well
+/// TODO: When the land is registered, the land department issues a Chanote
+/// TODO: When a condo unit is registered, the land department issues a Chanote as well
 #[derive(new, Getters, From, Into, Ord, PartialOrd, Eq, PartialEq, Default, Hash, Clone, Debug)]
 pub struct ThailandLandDepartment {}
 
